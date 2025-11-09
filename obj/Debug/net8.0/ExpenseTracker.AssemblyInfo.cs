8704d9db51f9e7ef09a1186463fdf7f91f3e6b06
ShikharSrivastava25 <srivastava.shikhar2511@gmail.com>
2025-11-09 23:38:14 +0530

    Initial commit

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ee3c7cbe79ffe06d43269b3a27a0347a2bffb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
